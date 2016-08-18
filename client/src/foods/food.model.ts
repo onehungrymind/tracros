eef269f77f405e9cdeb74e24aaef179bac845c93
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-08-18 10:34:14 -0700

    cleaning for deploy

--- client/src/foods/food.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Food {
-  $key?: string;
-  name: string;
-  protein?: number;
-  fat?: number;
-  carbs?: number;
-  calories?: number;
-};
--- client/src/profile/mockProfile.ts
+++
@@ -1,8 +0,0 @@
-export const profile: any = {
-  metrics: [
-    {name: 'Protein', threshold: '1000'},
-    {name: 'Fat', threshold: '1000'},
-    {name: 'Carbs', threshold: '1000'},
-    {name: 'Calories', threshold: '1000'}
-  ]
-};
